222c726f643a957d367b45602dabf2ebe1ccead3
noicht <tincho_cabrera98@outlook.com>
2020-07-16 18:41:29 -0300

    cambio 16/7

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -1,8 +1,10 @@
+/* */
 /*
 Al presionar el  botón, 
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Dame las drogas, Lisa");
+	alert("Vas a morir, Moe");
 }
+
 
