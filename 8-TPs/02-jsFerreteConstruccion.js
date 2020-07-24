0491a539c14d7be731555342e22b6d70ca31ad0c
noicht <tincho_cabrera98@outlook.com>
2020-07-23 22:45:51 -0300

    Carga de tp de E/S 23/07/2020

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -6,13 +6,45 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largo;
+	var ancho;
+	var result;
+
+	largo = parseFloat(document.getElementById("txtIdLargo").value);
+	ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+	result = ((largo + ancho)*2)* 3; 
+
+	alert("Cantidad de alambre a comprar: " + result + " mts");
 
 }
 function Circulo () 
 {
+	var radio;
+	var result;
+
+	radio = parseFloat(document.getElementById("txtIdRadio").value);
 	
+	result = (Math.PI * (radio * radio)) * 3;
+
+	alert("Cantidad de alambre a comprar: " + result.toFixed(2) + " mts")
+
 }
 function Materiales () 
 {
+	var largo;
+	var ancho;
+	var bolsaCemento;
+	var bolsaCal;
+
+	largo = parseFloat(document.getElementById("txtIdLargo").value);
+	ancho = parseFloat(document.getElementById("txtIdAncho").value);
+
+	bolsaCemento = (largo * ancho) * 2;
+	bolsaCal = (largo * ancho) * 3;
+
+	alert("Se necesitan " + bolsaCemento + " bolsas de cemento y " + bolsaCal + " bolsas de cal");
+
+
 	
 }
